2546a747beba371aae3e9de875b3b3e63cea4e47
plh <hieuflong@gmail.com>
2021-07-10 17:41:29 +0700

    Them trang quan ly chuyen muc cho admin

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -3,5 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', adminPage);
+router.get('/quanlycate', function(req, res){
+    res.render('user/admin/quanlycate')
+});
 
 module.exports = router;
