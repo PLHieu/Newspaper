b6c749d3e3ea883bfbb543339a0a1cff5622e99f
plh <hieuflong@gmail.com>
2021-07-11 08:50:59 +0700

    them giao dien cho trang quan ly bai viet

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,9 +2,17 @@ const { adminPage } = require("../controllers/testuser.controllers");
 const express = require('express');
 const router = express.Router();
 
-router.get('/', adminPage);
-router.get('/quanlycate', function(req, res){
-    res.render('user/admin/quanlycate')
+router.get('/manageuser', function(req, res){
+    return res.render('user/admin/quanlyuser')
+});
+router.get('/managecategory', function(req, res){
+    return res.render('user/admin/quanlycate')
+});
+router.get('/managepost', function(req, res){
+    return res.render('user/admin/quanlybaiviet')
+});
+router.get('/', function(req, res){
+    return res.redirect('/admin/manageuser')
 });
 
 module.exports = router;
